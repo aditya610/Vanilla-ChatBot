c65ec9e50877518503b7820c793d6b57bc2fe9ec
Japteg Singh <japteg.2698@gmail.com>
2017-07-10 17:15:11 +0530

    Update help message

--- services/helpService.js
+++ services/helpService.js
@@ -4,10 +4,12 @@ let message = "";
 
 message = `You Can :\n`
 message += `#1 Ask for a joke \n`
-message += `\t(cmd : joke) \n\n`
+message += `\t(command : joke) \n\n`
 message += `#2 Ask for weather conditions \n`
 message += `\t(for eg: whats the weather like in New York) \n\n`
-message += `#3 If you are feeling lonely we can simply talk \n`
+message += `#3 Ask for a place details \n`
+message += `\t(command : info <place name>, for eg : info eiffel tower) \n\n`
+message += `#4 If you are feeling lonely we can simply talk \n`
 message += `\t(for eg : hi , wassup?)`
 
 module.exports = message;
